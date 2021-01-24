f98458df52fa5f2e2bbe6011975067a35e73ffa5
Time&Space <617540836@qq.com>
2021-01-25 00:31:56 +0800

    删除无用空行

--- App/src/main/java/com/kotlin/mall/common/MainApplication.kt
+++ App/src/main/java/com/kotlin/mall/common/MainApplication.kt
@@ -9,10 +9,6 @@ import com.kotlin.base.common.BaseApplication
 class MainApplication: BaseApplication() {
     override fun onCreate() {
         super.onCreate()
-
-
-
-
         //极光推送初始化
         JPushInterface.setDebugMode(true)
         JPushInterface.init(this)
